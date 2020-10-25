50c495100c827ef38d0c12b9514ebe5ccf8cf170
Liza Krasnoshchok <nlight1122@gmail.com>
2020-10-25 20:19:50 +0200

    Updates

---
+++ precache-manifest.11be2e36d26047aabf6483f2a2067724.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc7684642c29d5310f16e1e4e0c29606",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e47e7e153c2697e9ef2a",
+    "url": "./static/css/main.4079c8c9.chunk.css"
+  },
+  {
+    "revision": "909add997e3a4f856304",
+    "url": "./static/js/2.f71748e5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.f71748e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e47e7e153c2697e9ef2a",
+    "url": "./static/js/main.6904c14d.chunk.js"
+  },
+  {
+    "revision": "ec438ca3dae444460a4d",
+    "url": "./static/js/runtime-main.1d48480d.js"
+  },
+  {
+    "revision": "c5be9cc0e0463249746b97f939e8951d",
+    "url": "./static/media/justDoIt.c5be9cc0.png"
+  }
+]);
